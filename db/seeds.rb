a59f81d37735eebe3a2c57a70364a8a64b90e8dc
masaki noguchi <masakinoguchi1@gmail.com>
2018-05-16 17:12:55 +0100

    yay

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Durka Durk',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '510-417-9744',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      'Ho Lee Fuk Ln, London E1 6PQ',
+    phone_number: '626-414-9744',
+    category:     'italian'
+  },
+  {
+    name:         'Meow Meow',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '626-100-7614',
+    category:     'japanese'
+  },
+  {
+    name:         'Tooki',
+    address:      '222 Drive Avenue, London E1 5G1',
+    phone_number: '523-114-2234',
+    category:     'belgian'
+  },
+  {
+    name:         'Quik Mafs',
+    address:      'Sum Ting Wong Dr, London E1 6PQ',
+    phone_number: '655-422-0044',
+    category:     'chinese'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
